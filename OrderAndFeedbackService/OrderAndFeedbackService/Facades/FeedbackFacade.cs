b9c745fb9e0d069c50e929b10cb2517cf42dc1e5
hindsMarcus <100131450+hindsMarcus@users.noreply.github.com>
2024-11-25 13:31:29 +0100

    Create feedback done

--- OrderAndFeedbackService/OrderAndFeedbackService/Facades/FeedbackFacade.cs
+++ OrderAndFeedbackService/OrderAndFeedbackService/Facades/FeedbackFacade.cs
@@ -11,14 +11,21 @@ public class FeedbackFacade
         {
             _context = context;
         }
+
         
+        //Create feedback by order
         public Feedback CreateFeedback(FeedbackDTO feedbackDto)
         {
-            Feedback feedback = new Feedback(feedbackDto);
+            OrderFacade orderFacade = new OrderFacade(_context);
+            Order order = orderFacade.GetOrder(feedbackDto.OrderDTO.Id);
+            Feedback feedback = new Feedback(feedbackDto,order);
+            feedback.Order = order;
             _context.Feedbacks.Add(feedback);
             _context.SaveChanges();
             return feedback;
         }
+        
+        
         
         //Get all feedbacks by restaurant by orders
 
