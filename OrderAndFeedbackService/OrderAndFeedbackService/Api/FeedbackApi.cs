445ce4cf7661bcdf80cc8aa101efad2659a5675f
hindsMarcus <100131450+hindsMarcus@users.noreply.github.com>
2024-11-25 12:41:00 +0100

    Create feedback done

---
+++ OrderAndFeedbackService/OrderAndFeedbackService/Api/FeedbackApi.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderAndFeedbackService.DTOs;
+using OrderAndFeedbackService.Facades;
+
+namespace OrderAndFeedbackService.Api;
+[ApiController]
+[Route("api/[controller]")]
+public class FeedbackApi : ControllerBase
+{
+    private readonly FeedbackFacade _feedbackFacade;
+    
+    public FeedbackApi(FeedbackFacade feedbackFacade)
+    {
+        _feedbackFacade = feedbackFacade;
+    }
+    
+    // POST: api/Feedback
+    [HttpPost]
+    public IActionResult CreateFeedback([FromBody] FeedbackDTO feedbackDto)
+    {
+        try
+        {
+            FeedbackDTO createdFeedback = new FeedbackDTO(_feedbackFacade.CreateFeedback(feedbackDto));
+            return Ok(createdFeedback);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    
+    
+}
